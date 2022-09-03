6e650af900540d8cf1b0d1fefa10c5749601f14f
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-09-03 07:03:26 +0600

    show news thumbnail, title dynamically

--- js/app.js
+++ js/app.js
@@ -22,7 +22,32 @@ const displayCategory = categorys => {
 const categoryId = async (id) => {
     const response = await fetch(`https://openapi.programming-hero.com/api/news/category/${id}`);
     const data = await response.json();
-    console.log(data.data);
+    displayCategoryNews(data.data);
+}
+const displayCategoryNews = allnews => {
+    const allNewsDetail = document.getElementById('all-news-detail')
+    allNewsDetail.innerHTML = '';
+    allnews.forEach(news => {
+        console.log(news)
+        const newsItem = document.createElement('div');
+        newsItem.classList.add('card')
+        newsItem.classList.add('mb-3')
+        newsItem.innerHTML = `
+        <div class="row g-0">
+            <div class="col-md-3">
+                <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-9">
+                <div class="card-body">
+                    <h5 class="card-title mb-4">${news.title}</h5>
+                    <p class="card-text">${news.details}</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>    
+        `
+        allNewsDetail.appendChild(newsItem);
+    })
 }
 
 loadCategory();
