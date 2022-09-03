a0e313f247dd6a23e19aed6e4d3aa34abea395b7
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-09-04 05:38:25 +0600

    added blog.html and modal

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 //Fetch Category Data From API
 const loadCategory = async () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
-    const res = await fetch(url);
-    const data = await res.json();
-    displayCategory(data.data.news_category);
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayCategory(data.data.news_category);
+    }
+    catch {
+        console.log(error);
+    }
 }
 
 const displayCategory = categorys => {
@@ -13,7 +18,7 @@ const displayCategory = categorys => {
         const categoryItem = document.createElement('div');
         categoryItem.classList.add('col');
         categoryItem.innerHTML = `
-        <div onclick="categoryId('${category.category_id}')">${category.category_name}</div>
+        <button onclick="categoryId('${category.category_id}')" type="button" class="btn btn-light">${category.category_name}</button>
         `
         allCategories.appendChild(categoryItem);
     })
@@ -27,9 +32,9 @@ const categoryId = async (id) => {
 }
 const displayCategoryNews = allnews => {
     const allNewsDetail = document.getElementById('all-news-detail')
+    loading(false);
     allNewsDetail.innerHTML = '';
     allnews.forEach(news => {
-        console.log(news)
         const newsItem = document.createElement('div');
         newsItem.classList.add('card')
         newsItem.classList.add('mb-3')
@@ -52,14 +57,27 @@ const displayCategoryNews = allnews => {
                     <p class="me-2"><i class="fa-regular fa-eye"></i></p>
                     <p class="fw-bold">${news.total_view ? news.total_view : 'No data found'}</p>
                   </div>
-                  <div><p class= "text-primary fs-4 text"><i class="fa-solid fa-arrow-right"></i></p></div>
+                    <div class= "text-primary fs-4 text border border-0" >
+                    <button onclick="loadNewsDetail('${news}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newsDetailsModal">
+                    <i class="fa-solid fa-arrow-right"></i>
+                </button>
+                  </div>
              </div>
             </div>
         </div>    
-        `
+          `
         allNewsDetail.appendChild(newsItem);
     })
-    loading(false);
+}
+
+const loadNewsDetail = async (newsId) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/news/category/${newsId}`);
+    const data = await response.json();
+    displayNewsDetails(data.data);
+}
+
+const displayNewsDetails = newsDetails => {
+    console.log(newsDetails)
 }
 
 
